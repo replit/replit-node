877e517907f44521bc082394b27b3dda65d5126f
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-10-31 18:06:55 -0700

    Refactor headerProxy into utils file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import * as express from "express";
 import { AuthMiddleware, RequestAuthContext } from "./types";
-
-function headerProxy(
-  req: express.Request,
-  obj: Record<string, any>,
-  prop: string,
-  headerName: string
-): void {
-  Object.defineProperty(obj, prop, {
-    configurable: false,
-    enumerable: true,
-    get: function () {
-      return req.headers[headerName];
-    },
-  });
-}
+import { headerProxy } from "./util";
 
 export const authMiddleware: AuthMiddleware = (req, _res, next) => {
   if (req.headers["x-replit-user-id"]) {
