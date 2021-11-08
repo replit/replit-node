a84f47ed378adbc366e68006f76a60ef9e1fca9d
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-11-07 16:35:44 -0800

    Implement low-level database API with undici

---
+++ src/db/rawclient.ts
@@ -0,0 +1,73 @@
+import { request } from "undici";
+
+export class KeyError extends Error {}
+
+/** Raw, low-level API access. */
+export class RawClient {
+  /** URL should not have a trailing slash, but it doesn't really matter. */
+  readonly url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  async get(key: string): Promise<string> {
+    const { statusCode, body } = await request(
+      this.url + "/" + encodeURIComponent(key)
+    );
+    if (statusCode == 404) {
+      throw new KeyError("Key not found");
+    }
+    if (statusCode != 200) {
+      throw new Error(
+        `Unexpected status code while getting key: ${statusCode}`
+      );
+    }
+    return body.text();
+  }
+
+  async setMany(items: Map<string, string>): Promise<void> {
+    const requestBodyItems = [];
+    for (const [key, val] of items.entries()) {
+      requestBodyItems.push(
+        encodeURIComponent(key) + "=" + encodeURIComponent(val)
+      );
+    }
+    const { statusCode } = await request(this.url, {
+      method: "POST",
+      body: requestBodyItems.join("&"),
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+    if (statusCode != 200) {
+      throw new Error(
+        `Unexpected status code while setting keys: ${statusCode}`
+      );
+    }
+  }
+
+  async delete(key: string): Promise<void> {
+    const { statusCode } = await request(
+      this.url + "/" + encodeURIComponent(key),
+      { method: "DELETE" }
+    );
+    if (statusCode != 204) {
+      throw new Error(
+        `Unexpected status code while deleting key: ${statusCode}`
+      );
+    }
+  }
+
+  async list(prefix: string): Promise<string[]> {
+    const { statusCode, body } = await request(
+      this.url + "?encode=true&prefix=" + encodeURIComponent(prefix)
+    );
+    if (statusCode != 200) {
+      throw new Error(
+        `Unexpected status code while listing keys: ${statusCode}`
+      );
+    }
+    return (await body.text())
+      .split("\n")
+      .map((val) => decodeURIComponent(val));
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+export { RawClient } from "./db/rawclient";
 export { AUTH_SNIPPET, requireAuth } from "./web";
