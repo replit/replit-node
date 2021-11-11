2fcda7e4b0fafb654d1d289f69b7b31e04e54b38
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-11-11 12:44:11 -0800

    Implement error handling for DB expose

--- src/db/server.ts
+++ src/db/server.ts
@@ -1,14 +1,41 @@
 import { Router } from "express";
 import { IDatabaseClient, Middleware } from "../types";
+import { KeyError } from "./rawclient";
+
+// Handles async errors by passing them to the error handler middleware.
+// Without this, the request will simply hang indefinitely on error.
+// express 5.0 does this out of the box but we want to support old versions
+function wrapAsync(func: Middleware): Middleware {
+  return async (req, res, next) => {
+    try {
+      return await func(req, res, next);
+    } catch (e) {
+      return next(e);
+    }
+  };
+}
 
 export function exposeRead(db: IDatabaseClient, keys: Set<string>): Middleware {
   const router = Router();
-  router.get("/:key", async (req, res, next) => {
-    const { key } = req.params;
-    if (!keys.has(key)) return next();
-    const value = await db.get(key);
-    res.json({ value });
-  });
+  router.get(
+    "/:key",
+    wrapAsync(async (req, res, next) => {
+      const { key } = req.params;
+      if (!keys.has(key)) return next();
+
+      let value;
+      try {
+        value = await db.get(key);
+      } catch (e) {
+        if (e instanceof KeyError) {
+          return res.status(404).json({ error: "Key not found" });
+        }
+        throw e;
+      }
+
+      return res.json({ value });
+    })
+  );
   return router;
 }
 
@@ -17,16 +44,21 @@ export function exposeWrite(
   keys: Set<string>
 ): Middleware {
   const router = Router();
-  router.post("/:key", async (req, res, next) => {
-    const { key } = req.params;
-    if (!keys.has(key)) return next();
-    const val = req.body.value;
-    if (typeof val !== "string") {
-      return res.status(400).json({ error: "Missing value in request body" });
-    }
-    await db.setMany(new Map([[key, val]]));
-    return res.json({ ok: true });
-  });
+  router.post(
+    "/:key",
+    wrapAsync(async (req, res, next) => {
+      const { key } = req.params;
+      if (!keys.has(key)) return next();
+
+      const val = req.body.value;
+      if (typeof val !== "string") {
+        return res.status(400).json({ error: "Missing value in request body" });
+      }
+
+      await db.setMany(new Map([[key, val]]));
+      return res.json({ ok: true });
+    })
+  );
   return router;
 }
 
